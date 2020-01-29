ee3d2e8d55211c0527402a9e700999eb6e349179
nouvelutilisateur1 <adil.haddad.xdev@gmail.com>
2020-01-29 01:18:24 +0100

    add and update function to disign and create different position for canvas floor .

--- js/gameManager.js
+++ js/gameManager.js
@@ -1,46 +1,78 @@
+var rectWith = 100 ;
+var rectHeight = 100 ;
+var startPosX = 0;
+var startPosY = 0;
+var startLigne = 1 ;
+var startCol = 5 ;
+var tab = [] ;
+var position = {x : 0 , y : 0} ;
+var nbrRect = 25 ;
+var canvWith = 500 ;
+var canvHeight = 500 ;
+
 $(document).ready(function() {
+    var p = createDefaultPositions() ;
+    createRondomPositions(p) ;
     var cnv=document.getElementById("canv");
     var ctx=cnv.getContext("2d");
+    cnv.addEventListener("mousedown", function () {
+        (function () {
+            var rect = cnv.getBoundingClientRect() ;
+            var x = event.clientX -  rect.left ;
+            var y = event.clientY - rect.top ;
+            console.log("Coordinate x: " + x,
+                "Coordinate y: " + y);
+            $.each(p , function (k , v) {
+                 if (contains(p,x,y)) {
+                     ctx.fillStyle = "green" ;
+                     ctx.fillRect(v.x , v.y ,rectWith , rectHeight);
+                 }
+            }) ;
 
-    var p = createPositions() ;
+        })(cnv , event , p);
+    }) ;
 
-    for (var i = 0 ; i < p.length  ; i++){
+    disignRect(0 , p , ctx);
 
-        window.setTimeout(createRect(ctx,i,p) ,3000) ;
 
-    }
+
 
 });
 
+function createRondomPositions(p) {
 
-function createRect(ctx,idx,p){
-    for ( i = idx ; i < p.length  ; i++){
-        ctx.fillStyle="yellow";
-        ctx.strokeStyle="black";
-        ctx.textAlign="center";
-        ctx.textBaseline="middle";
-        ctx.lineWidth=0.5;
-        ctx.fillRect(p[i].x , p[i].y , 100 , 100) ;
-        ctx.strokeRect(p[i].x , p[i].y , 100 , 100);
-    }
+        for (let i = p.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [p[i], p[j]] = [p[j], p[i]];
+        }
 }
 
-function createPositions(){
-    var rectWith = 100 ;
-    var rectHeight = 100 ;
-    var startPosX = 0;
-    var startPosY = 0;
-    var startLigne = 1 ;
-    var startCol = 5 ;
-    var tab = [] ;
-    var position = {x : 0 , y : 0} ;
-    var nbrRect = 25 ;
-    var canvWith = 500 ;
-    var canvHeight = 500 ;
+function disignRect(i , tabPos , ctx) {
+    setTimeCreation =  setTimeout(function () {
+
+        if (i >= 0 && i <= tabPos.length -1){
+            ctx.fillStyle="yellow";
+            ctx.strokeStyle="black";
+            ctx.textAlign="center";
+            ctx.textBaseline="middle";
+            ctx.lineWidth=0.5;
+            ctx.fillRect(tabPos[i].x , tabPos[i].y , 100 , 100) ;
+            ctx.strokeRect(tabPos[i].x , tabPos[i].y , 100 , 100);
+
+            i++ ;
+            disignRect(i , tabPos , ctx);
+
+        }
+
+    }, 500) ;
+}
+
+function createDefaultPositions(){
+
     for (var i = 0 ; i < nbrRect ; i ++){
 
         if ((startPosX < canvWith) && (startLigne == 1)){
-            var p ={ x : startPosX , y : startPosY } ;
+            var p ={ x : startPosX , y : startPosY  , width : 100 , height :100} ;
             tab.push(p);
 
         }else{
@@ -62,4 +94,13 @@ function createPositions(){
 
     }
     return tab ;
+}
+
+function contains(p , x , y) {
+    $.each(p , function (k , v) {
+        if (x >= v.x &&
+            x <= v.x + v.width &&
+            y >= v.y &&
+            y <= v.y + v.height ) return true ;
+    }) ;
 }
